f9a22521d482eb529447c679f4e0154c3da82a74
ArieshaMittal <ariesha.mittal@medly.com>
2021-08-17 19:27:30 +0530

    changes reverted

--- test/src/ListOfShows.test.ts
+++ test/src/ListOfShows.test.ts
@@ -12,15 +12,6 @@ describe("Movie Schedules Test", () => {
         ])
     })
 
-    it("should return all the timings available for a particular movie", () => {
-        const movie = listOfAvailableShows(currentState, "Incepion")
-        expect(movie).toEqual([
-            "88 seats are available on 1-08-20 in NIGHT show",
-            "11 seats are available on 3-08-20 in AFTERNOON show",
-            "20 seats are available on 5-08-20 in EVENING show"
-        ])
-    })
-
     it("should return empty list if movie is not available", () => {
         const movie = listOfAvailableShows(currentState, "Interstellar")
         expect(movie).toEqual([])
